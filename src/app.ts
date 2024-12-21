0c63070801caa53c69e16a4e478779b64db1cbb3
Rafi Ferdos <rafiferdos@gmail.com>
2024-12-21 12:13:16 +0600

    refactor: standardize code formatting and improve readability

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,19 @@
-import cors from "cors";
+import cors from 'cors'
 import express, { Application, Request, Response } from 'express'
-import router from "./app/routes";
+import router from './app/routes'
 
-const app: Application = express();
+const app: Application = express()
 
 app.use(express.json())
 app.use(cors())
 
 // Application Routes
 app.use('/api/v1', router)
-app.get('/', function(req: Request, res: Response) {
-    res.send('Hello Duniya')
+app.get('/', function (req: Request, res: Response) {
+  res.send('Hello Duniya')
 })
 
 app.use(globalErrorHandler)
 app.use(notFound)
 
-export default app;
+export default app
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,13 +1,11 @@
-import { Router } from "express";
+import { Router } from 'express'
 
 const router = Router()
 
-const moduleRoutes = [
+const moduleRoutes = []
 
-]
-
-moduleRoutes.forEach(route => {
-    router.use(route.path, route.route)
+moduleRoutes.forEach((route) => {
+  router.use(route.path, route.route)
 })
 
 export default router
