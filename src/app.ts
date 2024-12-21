4f4ea26d372ebb1056aa0811d478283ed64ec141
Rafi Ferdos <rafiferdos@gmail.com>
2024-12-21 12:00:18 +0600

    implement basic Express server with CORS and error handling

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import cors from "cors";
+import express, { Application, Request, Response } from 'express'
+
+const app: Application = express();
+
+app.use(express.json())
+app.use(cors())
+
+// Application Routes
+app.use('/api/v1', router)
+app.get('/', function(req: Request, res: Response) {
+    res.send('Hello Duniya')
+})
+
+app.use(globalErrorHandler)
+app.use(notFound)
+
+export default app;
