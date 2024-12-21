1086a7323971ba21c95e397c3fd4575b7aad8a4d
Rafi Ferdos <rafiferdos@gmail.com>
2024-12-21 12:13:28 +0600

    feat: add global error handling middleware and not found handler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import cors from 'cors'
 import express, { Application, Request, Response } from 'express'
+import globalErrorHandler from './app/middlewares/globalErrorHandler'
+import notFound from './app/middlewares/notFound'
 import router from './app/routes'
 
 const app: Application = express()
